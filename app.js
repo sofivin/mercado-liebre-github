10ae5b9409a8b07b7d463e3fd48d5a8922814f0a
sofivin <sofivin5@gmail.com>
2021-06-22 02:27:36 -0300

    Subiendo proyecto a git

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+// app.listen(3000, ()=>{
+//     console.log('Servidor funcionando');
+// });
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Servidor corriendo en el puerto 3000');
+})
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
